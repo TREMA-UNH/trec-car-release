7d415f5d228d16dbeedf2c4e485f827497f43f25
Ben Gamari <ben@smart-cactus.org>
2017-09-01 17:48:54 -0400

    multiling support

---
+++ multi-ling-car.it.mk
@@ -0,0 +1,7 @@
+bin=~/trec-car/mediawiki-annotate-release/bin
+root_url=http://dumps.wikimedia.your.org/enwiki/20161220
+embeddings=glove.6B.300d.txt
+lang=it
+version=v1.6
+dumpdate=20170801
+releasecomment="Multilingual CAR for Italian"
