505151d2cb4f6ce209635ba03a1717895aca1cbd
Ben Gamari <ben@smart-cactus.org>
2017-09-20 16:33:56 -0400

    Split out trec-car configuration

---
+++ trac-car.en.mk
@@ -0,0 +1,8 @@
+#!make -f
+
+bin=~/trec-car/mediawiki-annotate-release/bin
+root_url=http://dumps.wikimedia.your.org/enwiki/20161220
+embeddings=glove.6B.300d.txt
+version=v1.5.1
+
+include trec-car.mk
