75e5633dfb6484aaf217961b5aed23ef21adbd29
Ben Gamari <ben@smart-cactus.org>
2017-12-07 20:03:53 -0500

    Delete Make build system

--- trac-car.en.mk
+++
@@ -1,8 +0,0 @@
-#!make -f
-
-bin=~/trec-car/mediawiki-annotate-release/bin
-root_url=http://dumps.wikimedia.your.org/enwiki/20161220
-embeddings=glove.6B.300d.txt
-version=v1.5.1
-
-include trec-car.mk
