e7ed419d21776632e51f03aba1430af60e3255d3
Ben Gamari <ben@smart-cactus.org>
2017-12-29 14:33:28 -0500

    Fix cores option

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 name=$1
 shift
-nix build -f . -o result-$name $name --option max-cores 8 --option max-jobs 16 $@
+nix build -f . -o result-$name $name --option cores 8 --option max-jobs 16 $@
 
 # Set mtime
 touch result-$name
