565f3ac0705d42fbb7315c5583000495de7ac917
Ben Gamari <ben@smart-cactus.org>
2018-01-12 14:36:28 -0500

    update tools: redirect stdin to avoid hanging

--- update-tools.sh
+++ update-tools.sh
@@ -7,7 +7,7 @@ tools=$(nix eval --raw -f . carToolFiles)
 
 mkdir -p car-tools
 for tool in $tools; do
-    version=$($bin/$tool --tool-version 2>/dev/null || echo 0)
+    version=$($bin/$tool --tool-version 0>/dev/null 2>/dev/null || echo 0)
     cp -L $bin/$tool car-tools/$tool.$version
     rm -f car-tools/$tool
     ln -s $(realpath car-tools)/$tool.$version car-tools/$tool
