6f80818c71eb549db36401df5c490e6f868ed61e
Ben Gamari <ben@smart-cactus.org>
2022-02-21 22:09:26 -0500

    collect archives

--- collectArchives.sh
+++ collectArchives.sh
@@ -1,6 +1,8 @@
-cd main
+./run.sh main --cores 30 --max-jobs 4 --arg configFile ./config.${1}.nix
+pushd result-main
 tar -chvf /home/ben/trec-car/data/wiki2022-${1}-unprocessedAllJsonl.tar unprocessedAllJsonl/*
 tar -chvf /home/ben/trec-car/data/wiki2022-${1}-unprocessedAllCbor.tar unprocessedAllCbor/*
 tar -chvf /home/ben/trec-car/data/wiki2022-${1}-collectionCbor.tar collectionCbor/*
 tar -chvf /home/ben/trec-car/data/wiki2022-${1}-collectionJsonl.tar collectionJsonl/* 
+popd
 
